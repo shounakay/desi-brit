6bd222aa401c03a2c0dbdb8411e5c428d36bd08f
Shounak Chavan <chavanshounak24@gmail.com>
2023-09-23 13:54:59 +0530

    fix: Reduce footer logo left padd and remove duplicate asset files

--- src/pages/our-story/OurStory.tsx
+++ src/pages/our-story/OurStory.tsx
@@ -65,8 +65,8 @@ export const OurStory = () => {
             loading="lazy"
           />
         </section>
-        <footer className="flex justify-between items-center text-sm md:text-base px-4 lg:px-0 xl:px-10 bg-neutral-800 h-auto w-full py-6 text-stone-300">
-          <div className="justify-start basis-1/2 pl-2 md:pl-8 grow flex">
+        <footer className="flex justify-between items-center text-sm md:text-base pl-2 pr-4 lg:px-0 xl:pl-2 xl:pr-10 bg-neutral-800 h-auto w-full py-6 text-stone-300">
+          <div className="justify-start basis-1/2 pl-0 md:pl-8 grow flex">
             <img
               src="/assets/logo-balck-transp.png"
               alt="dark-log"
