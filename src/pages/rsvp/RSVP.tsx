16eb2984c70e794d7fdba6358e9d3f2447a8677d
Shounak Chavan <chavanshounak24@gmail.com>
2023-09-23 15:04:43 +0530

    chore: style rsvp

--- src/pages/rsvp/RSVP.tsx
+++ src/pages/rsvp/RSVP.tsx
@@ -69,7 +69,7 @@ export const RSVP = () => {
             </div>
           </nav>
           <nav className="lg:flex hidden justify-between items-center h-24 w-full text-stone-700 pl-6 pr-14">
-            <div className="flex flex-row gap-10 font-semibold text-lg">
+            <div className="flex flex-row gap-10 font-semibold text-base">
               <NavLink to="/">DESI BRIT WEDDING</NavLink>
               <NavLink to="/our-story">OUR STORY</NavLink>
               <NavLink to="/faqs" className="">
